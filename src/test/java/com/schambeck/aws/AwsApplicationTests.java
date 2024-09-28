4f42d766e942d85d69cb758cc9a8bafe4e495c40
Marcelo Schambeck <mschambeck@gmail.com>
2024-09-28 01:06:22 -0300

    Initial commit

---
+++ src/test/java/com/schambeck/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schambeck.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAwsApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
