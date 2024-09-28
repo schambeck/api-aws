2446bc0f949de74c637497eff17b55b2b2ce4a36
Marcelo Schambeck <mschambeck@gmail.com>
2024-09-28 08:04:27 -0300

    Docker

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 api-aws
+docker run -d --name api-aws -p 8080:8080 593793055971.dkr.ecr.sa-east-1.amazonaws.com/schambeck/api-aws
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,2 +1,6 @@
 #docker kill $(docker ps -q)
+#docker rm $(docker ps -aq)
 #docker rmi -f $(docker images -aq)
+
+aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 593793055971.dkr.ecr.sa-east-1.amazonaws.com
+docker pull 593793055971.dkr.ecr.sa-east-1.amazonaws.com/schambeck/api-aws:latest
